c44f9b27e500115449791a40318617e40b141c2e
marcos <marcosaureliofarias2023@gmail.com>
2022-09-24 23:36:42 -0300

    fullcycle

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+
+    <nav>
+      <div class="nav-wrapper blue darken-1">
+        <a href="#" class="brand-logo center">Produtos Track.co</a>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <form>
+
+          <label>Nome</label>
+          <input type="text" placeholder="Nome">
+          <label>Quantidade</label>
+          <input type="number" placeholder="QTD">
+          <label>Valor</label>
+          <input type="text" placeholder="Valor">
+
+          <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
+
+      </form>
+
+      <table>
+
+        <thead>
+
+          <tr>
+            <th>NOME</th>
+            <th>QTD</th>
+            <th>VALOR</th>
+            <th>OPÇÕES</th>
+          </tr>
+
+        </thead>
+
+        <tbody>
+
+          <tr v-for="produto of produtos" :key="produto.id">
+
+            <td>{{produto.nome}}</td>
+            <td>{{produto.quantidade}}</td>
+            <td>{{produto.valor}}</td>
+            <td>
+              <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
+              <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
+            </td>
+
+          </tr>
+
+        </tbody>
+      
+      </table>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import Produtos from './service/produtos.js'
+export default {
+
+  data() {
+    return {
+      produtos: []
+    }
+  },
+
+  mounted() {
+    
+    Produtos.listar().then(resposta => {
+      console.log(resposta.data);
+      this.produtos = resposta.data
+    })
+  }
+}
+
+</script>
+<style>
+
+</style>
---
+++ src/service/produtos.js
@@ -0,0 +1,7 @@
+import {http} from './config.js'
+
+export default {
+    listar: () => {
+        return http.get('livros')
+    }
+}
